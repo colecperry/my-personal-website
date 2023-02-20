0002de0d86034b95858e3244495c30507db01801
Cole Perry <colecperry@gmail.com>
2023-02-20 15:46:29 -0800

    Added Styling with CSS

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const element = document.getElementById("Skills");
+element.remove(Skills);
